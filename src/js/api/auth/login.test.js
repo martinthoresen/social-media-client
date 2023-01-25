a362ac644ba382967733d8ce3ed6ffe4a1fa9505
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-01-26 00:01:52 +0100

    Updated login test to mock a resolved request.

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -26,14 +26,30 @@ class LocalStorageMock {
 
 global.localStorage = new LocalStorageMock();
 
+// A constant returning a mock resolved value from a fetch
+
+const mockFetchSuccess = jest.fn().mockResolvedValue({
+  ok: true,
+  json: jest.fn().mockResolvedValue({
+    name: "testuser",
+    email: "testuser@noroff.no",
+    banner: null,
+    avatar: null,
+    accessToken: "35345342tewrta564354wt2wtea34653",
+  }),
+});
+
+global.fetch = mockFetchSuccess;
+
 /**
  * A function that tests if function stores a token when user logs in with valid credentials
  */
+
 describe("login", () => {
   it("stores a token in localstorage when provided with valid login credentials", async () => {
     localStorage.clear();
-    await login("testuser@noroff.no", "");
+    await login("testuser@noroff.no", "testpassword");
     const token = localStorage.getItem("token");
-    expect(token).isTruthy();
+    expect(token).toBeDefined();
   });
 });
